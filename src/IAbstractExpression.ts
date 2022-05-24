f5aca7de6718391135a9bad81de61fa4e748d8e9
Dikowiec Sebastian <diko2077@gmail.com>
2022-05-24 20:41:53 -0300

    Corrección

--- src/IAbstractExpression.ts
+++ src/IAbstractExpression.ts
@@ -2,6 +2,6 @@ interface IAbstractExpression {
     // Todas las Terminal and Non-Terminal expressions implementarán
     // un metodo `interpret`
     interpret(): number
-}  
+}   
 
 
--- test/client.test.ts
+++ test/client.test.ts
@@ -2,11 +2,10 @@ import { Add } from "../src/Add";
 import { Numeral } from "../src/Numeral";
 import { Subtract } from "../src/Substract";
 
-test('Devolver valor 11', () => {
+test('01_La operacíon debe devolver como resultado 11', () => {
     const SENTENCE = '5 + 4 - 3 + 7 - 2'
 
     const TOKENS = SENTENCE.split(' ')
-    console.log(JSON.stringify(TOKENS))
 
     // Creación de un array de tipo IAbstractExpression
     const AST: IAbstractExpression[] = []
@@ -21,12 +20,10 @@ test('Devolver valor 11', () => {
     expect((AST_ROOT as IAbstractExpression).interpret()).toBe(11);
 });
 
-test('Devolver valor 16', () => {
+test('02_La operacíon debe devolver como resultado 16', () => {
     const SENTENCE = '8 - 3 + 7 - 1 + 5'
 
-
     const TOKENS = SENTENCE.split(' ')
-    console.log(JSON.stringify(TOKENS))
 
     // Creación de un array de tipo IAbstractExpression
     const AST: IAbstractExpression[] = []
@@ -41,12 +38,11 @@ test('Devolver valor 16', () => {
     expect((AST_ROOT as IAbstractExpression).interpret()).toBe(16);
 });
 
-test('Devolver valor 39', () => {
+test('03_La operacíon debe devolver como resultado 39', () => {
     const SENTENCE = '100 - 40 - 7 - 2 - 12'
 
 
     const TOKENS = SENTENCE.split(' ')
-    //console.log(JSON.stringify(TOKENS))
 
     // Creación de un array de tipo IAbstractExpression
     const AST: IAbstractExpression[] = []
@@ -59,4 +55,23 @@ test('Devolver valor 39', () => {
     const AST_ROOT = AST.pop()
 
     expect((AST_ROOT as IAbstractExpression).interpret()).toBe(39);
+});
+
+test('04_La operacíon debe devolver como resultado 58', () => {
+    const SENTENCE = '10 + 9 + 2 + 15 + 22'
+
+
+    const TOKENS = SENTENCE.split(' ')
+
+    // Creación de un array de tipo IAbstractExpression
+    const AST: IAbstractExpression[] = []
+    AST.push(new Add(new Numeral(TOKENS[0]), new Numeral(TOKENS[2]))) // 10 + 9
+    AST.push(new Add(AST[0], new Numeral(TOKENS[4]))) // ^ + 2
+    AST.push(new Add(AST[1], new Numeral(TOKENS[6]))) // ^ + 15
+    AST.push(new Add(AST[2], new Numeral(TOKENS[8]))) // ^ + 22
+
+    // Utiliza la última fila de AST como nodo raíz
+    const AST_ROOT = AST.pop()
+
+    expect((AST_ROOT as IAbstractExpression).interpret()).toBe(58);
 });
